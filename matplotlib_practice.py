a95d8dbd1d68fa907b8a2353e52fbf3733ac61ee
Sri Ram Sharma <sriram.x108@gmail.com>
2025-03-19 22:11:21 +0530

    first commit

---
+++ matplotlib_practice.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+foods = ['Meat','Banana','Avocados','Sweet Potatoes','Spinach','Watermelon','Coconut Water','Beans','Legumes','Tomato']
+calories = [250,130,140,120,20,20,10,50,40,19]
+potassium = [40,55,20,30,40,32,10,26,25,20]
+fat = [8,5,3,6,1,1.5,0,2,1.5,2.5]
